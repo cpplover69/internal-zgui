2dc3817e97985753083a110e8592415c907121c4
nom <68795165+cpplover69@users.noreply.github.com>
2021-06-16 10:37:10 -0700

    Add files via upload

---
+++ zgui internal/AndroidUpdated/configs.cpp
@@ -0,0 +1,22 @@
+/*
+This source is free on githuh: https://github.com/Android1337/Basic-Fortnite-Cheat-Source-Internal
+If you sell this I will make you famous (in a bad way)
+nom is very cool and made the zgui part thing bc zgui look cool yes
+*/
+
+#include "include.h"
+
+CONFIGS Configs = { 0 };
+
+namespace InitSettings 
+{
+	VOID InitializeSettings()
+	{
+
+		//esp
+		Configs.Boxes = false;
+		Configs.Visuals = false;
+		Configs.Skeletons = false;
+		Configs.PlayerLines = false;
+	}
+}
